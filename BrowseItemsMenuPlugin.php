de944aa18d4f4463d06cb91d09830798b27f2364
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2018-06-30 11:08:35 -0500

    Initial commit

---
+++ BrowseItemsMenuPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class BrowseItemsMenuPlugin extends Omeka_Plugin_AbstractPlugin
+{
+  protected $_filters = array('public_navigation_items');
+
+  public function filterPublicNavigationItems($navArray)
+  {
+    $navArray = array(
+        array(
+            'label' => __('Browse All'),
+            'uri' => url('items/browse')
+        ),
+        array(
+            'label' => __('Stories'),
+            'uri' => url('items/browse?type=1')
+        ),
+        array(
+            'label' => __('Images'),
+            'uri' => url('items/browse?type=6')
+        ),
+        array(
+            'label' => __('Video'),
+            'uri' => url('items/browse?type=3')
+        ),
+        array(
+            'label' => __('Audio'),
+            'uri' => url('items/browse?type=5')
+        ),
+        array(
+            'label' => __('Search'),
+            'uri' => url('items/search')
+        )
+    );
+
+    return $navArray;
+  }
+
+}
+?>
